1011c29161e76e40700c532ba2e0cb70c8b86713
evan_yang <evan.yang@cn.schindler.com>
2018-01-29 21:47:22 +0800

    Sina01- We need to pass the Target and make it as a param , as we can use it to call the actioin operation. The action must related to the Target.

--- 微博项目/微博项目/Main/WeiBoNavigationController.m
+++ 微博项目/微博项目/Main/WeiBoNavigationController.m
@@ -19,15 +19,33 @@
     // Do any additional setup after loading the view.
 }
 
+/*
+ * We always use the function of "pushViewController: animated:" to catch the pushed
+ * viewcontroller, that we can change the behaviors for that.
+ */
+
 - (void)pushViewController:(UIViewController *)viewController animated:(BOOL)animated{
     [super pushViewController:viewController animated:animated];
     if (self.viewControllers.count>1) {
+        
+        /* It's better if we use the category of UIBarButtonItem , and put it in the
+         pch file , that we can use it to create the appropriate instance of UIBarButtonItem.
+         But the important things was that we need to also pass by value for "Target" self, because we use the the "Action" as method
+         */
+        
+        viewController.navigationItem.leftBarButtonItem = [UIBarButtonItem barButtonWithTarget:self Action:@selector(backBtnClicked:) Image:@"navigationbar_back" HighlightedImage:@"navigationbar_back_highlighted"];
+        
+        viewController.navigationItem.rightBarButtonItem = [UIBarButtonItem barButtonWithTarget:self Action:@selector(backToRootViewController:) Image:@"navigationbar_more" HighlightedImage:@"navigationbar_more_highlighted"];
+        
+        /*
         viewController.navigationItem.leftBarButtonItem = [self barButtonWithAction:@selector(backBtnClicked:) Image:@"navigationbar_back" HighlightedImage:@"navigationbar_back_highlighted"];
         
         viewController.navigationItem.rightBarButtonItem = [self barButtonWithAction:@selector(backToRootViewController:) Image:@"navigationbar_more" HighlightedImage:@"navigationbar_more_highlighted"];
+         */
     }
 }
 
+/*
 -(UIBarButtonItem *)barButtonWithAction:(SEL)actioin Image:(NSString *)image HighlightedImage:(NSString *)hightlightedImage{
     
     UIButton *backbtn = [UIButton buttonWithType:UIButtonTypeCustom];
@@ -39,7 +57,7 @@
     [backbtn addTarget:self action:actioin forControlEvents:UIControlEventTouchUpInside];
     
     return [[UIBarButtonItem alloc]initWithCustomView:backbtn];
-}
+}*/
 
 -(void)backBtnClicked:(UIButton *)btn{
     [self popViewControllerAnimated:YES];
