900728cfc4cd8bd5e4e4207f16ecf535cc38327c
evan_yang <evan.yang@cn.schindler.com>
2018-02-01 19:31:48 +0800

    Sina04-自定义searchBar，一个TextField，一个imageView，设置TextField的leftView为imageView，即指定的搜索Icon

--- 微博项目/微博项目/Discover/Controllers/DiscoverViewController.m
+++ 微博项目/微博项目/Discover/Controllers/DiscoverViewController.m
@@ -17,21 +17,20 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    UITextField *txtField = [[UITextField alloc]initWithFrame:CGRectMake(0, 0, self.view.size.width-20, 30)];
+    //需要去拉伸图片，直接使用它为背景very ugly
+    UIImage *img = [UIImage imageNamed:@"searchbar_textfield_background"];
+    UIImage *bgImg = [img stretchableImageWithLeftCapWidth:img.size.width*0.5 topCapHeight:img.size.height*0.5];
+    txtField.background = bgImg;
+    txtField.borderStyle = UITextBorderStyleNone;
+    self.navigationItem.titleView = txtField;
+    
+    UIImageView *imgView = [[UIImageView alloc]initWithFrame:CGRectMake(0, 0, 30, 30)];
+    imgView.contentMode = UIViewContentModeCenter;
+    imgView.image = [UIImage imageNamed:@"searchbar_textfield_search_icon"];
+    txtField.leftView = imgView;
+    txtField.leftViewMode = UITextFieldViewModeAlways;
 }
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
