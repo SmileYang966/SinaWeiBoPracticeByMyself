0cd1941133d2d680de226f45a86349220694f11c
evan_yang <evan.yang@cn.schindler.com>
2018-02-01 20:04:02 +0800

    Sina05 - 自定义并把UITextField封装成一个SearchBar，通过类方法searchBar去Get到一个可用的searchBar

--- 微博项目/微博项目/Discover/Views/SCSearchBar.m
+++ 微博项目/微博项目/Discover/Views/SCSearchBar.m
@@ -1,43 +1,31 @@
 //
-//  DiscoverViewController.m
+//  SCSearchBar.m
 //  微博项目
 //
-//  Created by Evan Yang on 23/01/2018.
+//  Created by Evan Yang on 01/02/2018.
 //  Copyright © 2018 Evan. All rights reserved.
 //
 
-#import "DiscoverViewController.h"
 #import "SCSearchBar.h"
 
-@interface DiscoverViewController ()
+@implementation SCSearchBar
 
-@end
-
-@implementation DiscoverViewController
-
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view.
++(instancetype)searchBar{
     
-    /*
-    UITextField *txtField = [[UITextField alloc]initWithFrame:CGRectMake(0, 0, self.view.size.width-20, 30)];
+    UITextField *txtField = [[UITextField alloc]init];
     //需要去拉伸图片，直接使用它为背景very ugly
     UIImage *img = [UIImage imageNamed:@"searchbar_textfield_background"];
     UIImage *bgImg = [img stretchableImageWithLeftCapWidth:img.size.width*0.5 topCapHeight:img.size.height*0.5];
     txtField.background = bgImg;
     txtField.borderStyle = UITextBorderStyleNone;
-    self.navigationItem.titleView = txtField;
     
     UIImageView *imgView = [[UIImageView alloc]initWithFrame:CGRectMake(0, 0, 30, 30)];
     imgView.contentMode = UIViewContentModeCenter;
     imgView.image = [UIImage imageNamed:@"searchbar_textfield_search_icon"];
     txtField.leftView = imgView;
     txtField.leftViewMode = UITextFieldViewModeAlways;
-     */
     
-    SCSearchBar *searchBar = [SCSearchBar searchBar];
-    searchBar.frame = CGRectMake(0, 0, self.view.size.width-20, 30);
-    self.navigationItem.titleView = searchBar;
+    return txtField;
 }
 
 @end
