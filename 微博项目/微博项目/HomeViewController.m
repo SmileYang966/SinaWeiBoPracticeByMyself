cd6bd92dd184dcf6247d5138012f2fd4935bfb8e
evan_yang <evan.yang@cn.schindler.com>
2018-02-04 18:41:41 +0800

    Sina05 - 新建一个下拉菜单，其中运用了两个比较重要的技术点：一个是拉伸的技术，另一个是如果保证一个view始终在当前view层次的最上层。我在代码中加入了详细的解释

--- 微博项目/微博项目/HomeViewController.m
+++ 微博项目/微博项目/HomeViewController.m
@@ -17,21 +17,42 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    self.navigationItem.leftBarButtonItem = [UIBarButtonItem barButtonWithTarget:self Action:@selector(leftBarButtonItemClicked:) Image:@"navigationbar_friendsearch" HighlightedImage:@"navigationbar_friendsearch_highlighted"];
+    
+    self.navigationItem.rightBarButtonItem = [UIBarButtonItem barButtonWithTarget:self Action:@selector(rightBarButtonItemClicked:) Image:@"navigationbar_pop" HighlightedImage:@"navigationbar_pop_highlighted"];
+    
+    UIButton *titleBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+    titleBtn.backgroundColor = [UIColor redColor];
+    NSDictionary *fontAttr = @{NSFontAttributeName : [UIFont systemFontOfSize:18.0]};
+    NSAttributedString *attrStr = [[NSAttributedString alloc]initWithString:@"首页" attributes:fontAttr];
+    [titleBtn setAttributedTitle:attrStr forState:UIControlStateNormal];
+    [titleBtn setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+    [titleBtn setImage:[UIImage imageNamed:@"navigationbar_arrow_down"] forState:UIControlStateNormal];
+    [titleBtn addTarget:self action:@selector(titleBtnClicked:) forControlEvents:UIControlEventTouchUpInside];
+    self.navigationItem.titleView = titleBtn;
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+//另外这里非常重要的一个知识点就是关于对图片的拉伸技术，
+//请到Assets.xcassets目录中去找到其对应的popover_background图片，然后对它的属性进行设置
+//Assets.xcassets -> find popover_background image -> Slicing -> Slices -> Vertical
+//这张图片我们只能垂直方向拉伸，因为水平方向是不规则的，一旦拉伸，原来的图片形状就变了
+//记住一个要点：做拉伸操作，只能对规则的那个方向(垂直还是水平)进行拉伸
+-(void)titleBtnClicked:(UIButton *)btn{
+    UIImageView *dropDownMenu = [[UIImageView alloc]initWithFrame:CGRectMake(0, 64, 217, 200)];
+    dropDownMenu.center = CGPointMake(self.view.center.x, dropDownMenu.center.y);
+    dropDownMenu.image = [UIImage imageNamed:@"popover_background"];
+    //如何保证我们当前的TitleBodyView始终在当前控制器的最上层
+    //事实上，我们可以手动创建多个UIWindow，但如何保证我们这个DropDownMenu始终保持在最上方呢
+    //这就需要用到下面的UIApplication的实例当中去取出最后一个也就是最上层的那个window
+    [[[UIApplication sharedApplication].windows lastObject] addSubview:dropDownMenu];
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (void)leftBarButtonItemClicked:(UIBarButtonItem *)item{
 }
-*/
+
+- (void)rightBarButtonItemClicked:(UIBarButtonItem *)item{
+}
+
 
 @end
